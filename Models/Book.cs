ac22ba1483a4633b97b6c709abcf12191de609a2
filipogit <andersson1_filip@hotmail.com>
2026-02-11 22:35:17 +0100

    Add Book model and test project; stub Loan and Member classes
    Added BiblioteksTest project to the solution. Implemented the Book class with required properties, constructor, and GetInfo() method. Added empty class definitions for Loan and Member in the Models namespace.

---
+++ Models/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotekssystem.Models
+{
+    public class Book
+    {
+        public Book(string iSBN, string title, string author, int publishedYear)
+        {
+            ISBN = iSBN;
+            Title = title;
+            Author = author;
+            PublishedYear = publishedYear;
+        }
+
+        public string ISBN { get; init; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublishedYear { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public string GetInfo()
+        {
+            return $"ISBN: {ISBN}, Title: {Title}, Author: {Author}, Published Year: {PublishedYear}, Available: {IsAvailable}";
+        }
+    }
+}
+
+/* Properties: ISBN(string), Title(string), Author(string), PublishedYear(int), IsAvailable(bool)
+Konstruktor som tar obligatoriska parametrar
+ISBN ska endast kunna sättas vid skapande
+Metod GetInfo() som returnerar formaterad bokinformation*/
