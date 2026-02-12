ff31d364280c05e5baf4a1f67e781ee8bced12c8
filipogit <andersson1_filip@hotmail.com>
2026-02-12 17:06:39 +0100

    Add xUnit test project and initial unit tests

---
+++ Bibliotekssystem.Test/LibraryStatisticsTests.cs
@@ -0,0 +1,93 @@
+﻿using Xunit;
+using Bibliotekssystem.Models;
+using Bibliotekssystem.Services;
+
+namespace BiblioteksTest
+{
+    public class LibraryStatisticsTests
+    {
+        [Fact]
+        public void GetTotalBookCount_ShouldReturnCorrectCount()
+        {
+            // Arrange
+            var library = new Library();
+            library.AddItem(new Book("123", "Bok 1", "Författare 1", 2020));
+            library.AddItem(new Book("456", "Bok 2", "Författare 2", 2021));
+            library.AddItem(new Magazine("M001", "Tidning", 2024, 1, "Förlag", "Jan"));
+            library.AddItem(new DVD("D001", "Film", 2022, "Regissör", 120, "Action"));
+
+            // Act
+            var totalBooks = library.GetTotalBookCount();
+
+            // Assert
+            Assert.Equal(2, totalBooks); // Endast böcker, inte tidningar eller DVD
+        }
+
+        [Fact]
+        public void GetBorrowedItemCount_ShouldReturnCorrectCount()
+        {
+            // Arrange
+            var library = new Library();
+            var book1 = new Book("123", "Bok 1", "Författare 1", 2020);
+            var book2 = new Book("456", "Bok 2", "Författare 2", 2021);
+            var book3 = new Book("789", "Bok 3", "Författare 3", 2022);
+
+            library.AddItem(book1);
+            library.AddItem(book2);
+            library.AddItem(book3);
+
+            // Låna ut två böcker
+            book1.CheckOut("Anna Andersson");
+            book2.CheckOut("Erik Eriksson");
+
+            // Act
+            var borrowedCount = library.GetBorrowedItemCount();
+
+            // Assert
+            Assert.Equal(2, borrowedCount);
+        }
+
+        [Fact]
+        public void GetMostActiveBorrower_ShouldReturnMemberWithMostLoans()
+        {
+            // Arrange
+            var library = new Library();
+            var book1 = new Book("123", "Bok 1", "Författare 1", 2020);
+            var book2 = new Book("456", "Bok 2", "Författare 2", 2021);
+            var book3 = new Book("789", "Bok 3", "Författare 3", 2022);
+
+            library.AddItem(book1);
+            library.AddItem(book2);
+            library.AddItem(book3);
+
+            // Anna lånar 2 böcker, Erik lånar 1
+            book1.CheckOut("Anna Andersson");
+            book2.CheckOut("Anna Andersson");
+            book3.CheckOut("Erik Eriksson");
+
+            // Act
+            var mostActive = library.GetMostActiveBorrower();
+
+            // Assert
+            Assert.Equal("Anna Andersson", mostActive);
+        }
+
+        [Fact]
+        public void SortByTitle_ShouldReturnAlphabeticalOrder()
+        {
+            // Arrange
+            var library = new Library();
+            library.AddItem(new Book("123", "Zebra", "Författare 1", 2020));
+            library.AddItem(new Book("456", "Apa", "Författare 2", 2021));
+            library.AddItem(new Book("789", "Björn", "Författare 3", 2022));
+
+            // Act
+            var sortedBooks = library.SortByTitle(ascending: true);
+
+            // Assert
+            Assert.Equal("Apa", sortedBooks[0].Title);
+            Assert.Equal("Björn", sortedBooks[1].Title);
+            Assert.Equal("Zebra", sortedBooks[2].Title);
+        }
+    }
+}
---
+++ Bibliotekssystem.Test/LoanTests.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+using Bibliotekssystem.Models;
+
+namespace BiblioteksTest
+{
+    public class LoanTests
+    {
+        [Fact]
+        public void IsOverdue_ShouldReturnFalse_WhenDueDateIsInFuture()
+        {
+            // Arrange
+            var book = new Book("123", "Test", "Author", 2024);
+            var member = new Member("M001", "Test Person", "test@test.com", DateTime.Now.AddYears(-1));
+            var loan = new Loan(book, member, DateTime.Now, DateTime.Now.AddDays(14));
+
+            // Act & Assert
+            Assert.False(loan.IsOverdue);
+        }
+
+        [Fact]
+        public void IsOverdue_ShouldReturnTrue_WhenDueDateHasPassed()
+        {
+            // Arrange
+            var book = new Book("123", "Försenad bok", "Author", 2024);
+            var member = new Member("M001", "Test Person", "test@test.com", DateTime.Now.AddYears(-1));
+            var loan = new Loan(book, member, DateTime.Now.AddDays(-20), DateTime.Now.AddDays(-5));
+
+            // Act
+            var isOverdue = loan.IsOverdue;
+
+            // Assert
+            Assert.True(isOverdue);
+        }
+
+        [Fact]
+        public void IsReturned_ShouldReturnTrue_WhenReturnDateIsSet()
+        {
+            // Arrange
+            var book = new Book("123", "Test", "Author", 2024);
+            var member = new Member("M001", "Test Person", "test@test.com", DateTime.Now.AddYears(-1));
+            var loan = new Loan(book, member, DateTime.Now.AddDays(-10), DateTime.Now.AddDays(4));
+
+            // Act
+            loan.ReturnBook();
+
+            // Assert
+            Assert.True(loan.IsReturned);
+            Assert.NotNull(loan.ReturnDate);
+        }
+    }
+}
